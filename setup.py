56f334487c7dd8614d4be2287187a0dea851a95e
tom-sapletta-com <tom@sapletta.com>
2025-05-28 10:30:50 +0200

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bexy",
-    version="0.1.12",
+    version="0.1.13",
     description="A sandbox for safely running Python code",
     author="Tom Sapletta",
     packages=find_packages(),
